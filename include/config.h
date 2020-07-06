8f02ee0f66d4f09a8ab7c031c9758f3e9e10d65c
Aldrin <akmontan@ucsc.edu>
2020-07-06 14:09:41 -0700

    Adding placeholder config header

---
+++ include/config.h
@@ -0,0 +1,9 @@
+// Some project-wide configuration (following the CMake Tutorial)
+
+#ifndef __KineticPrototype_H
+#define __KineticPrototype_H
+
+#define KineticPrototype_VERSION_MAJOR 0
+#define KineticPrototype_VERSION_MINOR 1
+
+#endif // __KineticPrototype_H
