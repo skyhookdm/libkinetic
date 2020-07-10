61a4ae3d0a5f86deff2d8f1ca28f1f6393e46d0c
Aldrin <akmontan@ucsc.edu>
2020-07-10 15:58:03 -0700

    Minor adjustments to style
    I changed tabs to spaces and vertically aligned some components

--- src/getlog.h
+++ src/getlog.h
@@ -19,75 +19,75 @@ typedef enum kgltype {
 
 
 typedef struct kutilization {
-	char *ku_name;
-	float ku_value;
+    char *ku_name;
+    float ku_value;
 } kutilization_t;
 
-typedef struct ktempratures {
-	char * kt_name;		/* Descriptive name, "HDA" "Processor" */
-	float ku_cur;		/* Current temp in C */
-	float ku_min;		/* Minimum temp in C */
-	float ku_max;		/* Maximum temp in C */
-	float ku_target;	/* Target temp in C */
-} ktempratures_t;
+typedef struct ktemperatures {
+    char * kt_name;     /* Descriptive name, "HDA" "Processor" */
+    float ku_cur;       /* Current temp in C */
+    float ku_min;       /* Minimum temp in C */
+    float ku_max;       /* Maximum temp in C */
+    float ku_target;    /* Target temp in C */
+} ktemperatures_t;
 
 typedef struct kcapacity {
-	uint64_t kc_total;	/* Total available bytes */
-	float kc_used;		/* Percent of Total used */
+    uint64_t kc_total;  /* Total available bytes */
+    float kc_used;      /* Percent of Total used */
 } kcapacity_t;
 
 typedef struct kinterface {
-	char *ki_name;	/* Interface Name, e.g "eth0" */ 
-	char *ki_mac;	/* MAC address, e.g. "00:15:5d:e2:ef:17" */ 
-	char *ki_ipv4;	/* IPv4 address. e.g. "192.168.200.33" */
-	char *ki_ipv6;	/* IPv6 address. e.g. "fe80::215:5dff:fee2:ef17" */
+    char *ki_name;  /* Interface Name, e.g "eth0" */
+    char *ki_mac;   /* MAC address, e.g. "00:15:5d:e2:ef:17" */
+    char *ki_ipv4;  /* IPv4 address. e.g. "192.168.200.33" */
+    char *ki_ipv6;  /* IPv6 address. e.g. "fe80::215:5dff:fee2:ef17" */
 } kinterface_t;
 
 typedef struct kconfiguration {
-	char *kcf_vendor;		/* Device Vendor Name  */
-	char *kcf_model;		/* Device Model Number */
-	char *kcf_serial;		/* Device Serial Number */
-	char *kcf_wwn;			/* Device World Wide Name */
-	char *kcf_version;		/* Device Version */
-	char *kcf_compdate;		/* Device Firmware Compilation Date */
-	char *kcf_srchash;		/* Firmware Source Code Hash */
-	char *kcf_proto;		/* Kinetic Protocol Version Number */
-	char *kcf_protocompdate;	/* Kinetic Protocol Compilation Date */
-	char *kcf_protosrchash;		/* Kinetic Protocol Source Code Hash */
-	kinterface_t *kcf_interfaces;	/* Device Interface List */
-	uint32_t kcf_port;		/* Device Unencrypted Port */
-	uint32_t kcf_tlsport;		/* Device Encrypted Port */
-	kpowerlevel_t kcf_power;	/* Device Current Power Level */
+    char *kcf_vendor;       /* Device Vendor Name  */
+    char *kcf_model;        /* Device Model Number */
+    char *kcf_serial;       /* Device Serial Number */
+    char *kcf_wwn;          /* Device World Wide Name */
+    char *kcf_version;      /* Device Version */
+    char *kcf_compdate;     /* Device Firmware Compilation Date */
+    char *kcf_srchash;      /* Firmware Source Code Hash */
+    char *kcf_proto;        /* Kinetic Protocol Version Number */
+    char *kcf_protocompdate;    /* Kinetic Protocol Compilation Date */
+    char *kcf_protosrchash;     /* Kinetic Protocol Source Code Hash */
+    kinterface_t *kcf_interfaces;   /* Device Interface List */
+    uint32_t kcf_port;      /* Device Unencrypted Port */
+    uint32_t kcf_tlsport;       /* Device Encrypted Port */
+    kpowerlevel_t kcf_power;    /* Device Current Power Level */
 } kconfiguration_t;
 
 typedef struct kstatistics {
-	kmtype_t ks_mtype;
-	uint64_t ks_cnt;
-	uint64_t ks_bytes;
+    kmtype_t ks_mtype;
+    uint64_t ks_cnt;
+    uint64_t ks_bytes;
 } kstatistics_t;
 
-/** 
- * klimits structure. 
- * These are all maximums. 
+/**
+ * klimits structure.
+ * These are all maximums.
  * structure elements that are "len"s are in bytes.
  * structure elements that are "cnt"s are in individual units
- */ 
+ */
 typedef struct klimits {
-	uint32_t kl_keylen;		/* Maximum Key Length supported */
-	uint32_t kl_vallen;		/* Maximum Value Length supported */
-	uint32_t kl_verlen;		/* Maximum Version Length supported */
-	uint32_t kl_taglen;		/* Maximum Tag Length supported */
-	uint32_t kl_msglen;		/* Maximum Message Length supported */
-	uint32_t kl_pinlen;		/* Maximum Pin Length supported */
-	uint32_t kl_batlen;		/* Maximum Batch Length supported */
-	uint32_t kl_pendrdcnt;		/* Total Allowed Pending RD Reqs */
-	uint32_t kl_pendwrcnt;		/* Total Allowed Pending WR Reqs */
-	uint32_t kl_conncnt;		/* Total Allowed Client Connections */
-	uint32_t kl_idcnt;		/* Total Allowed User IDs */
-	uint32_t kl_rangekeycnt;	/* Total Allowed Keys/Range Op */
-	uint32_t kl_batopscnt;		/* Total Allowed Ops/Batch */
-	uint32_t kl_batdelcnt;		/* Total Allowed Delete Ops/Batch */
-	uint32_t kl_devbatcnt;		/* Total Allowed Open Batches/Device */
+    uint32_t kl_keylen;         /* Maximum Key Length supported */
+    uint32_t kl_vallen;         /* Maximum Value Length supported */
+    uint32_t kl_verlen;         /* Maximum Version Length supported */
+    uint32_t kl_taglen;         /* Maximum Tag Length supported */
+    uint32_t kl_msglen;         /* Maximum Message Length supported */
+    uint32_t kl_pinlen;         /* Maximum Pin Length supported */
+    uint32_t kl_batlen;         /* Maximum Batch Length supported */
+    uint32_t kl_pendrdcnt;      /* Total Allowed Pending RD Reqs */
+    uint32_t kl_pendwrcnt;      /* Total Allowed Pending WR Reqs */
+    uint32_t kl_conncnt;        /* Total Allowed Client Connections */
+    uint32_t kl_idcnt;          /* Total Allowed User IDs */
+    uint32_t kl_rangekeycnt;    /* Total Allowed Keys/Range Op */
+    uint32_t kl_batopscnt;      /* Total Allowed Ops/Batch */
+    uint32_t kl_batdelcnt;      /* Total Allowed Delete Ops/Batch */
+    uint32_t kl_devbatcnt;      /* Total Allowed Open Batches/Device */
 } klimits_t;
 
 /*
@@ -96,30 +96,30 @@ typedef struct klimits {
  * supplied extensions, i.e. "com.WD.glog" would be a vendor spefic log
  */
 typedef struct kdevicelog {
-	char *kdl_name;
+    char *kdl_name;
 } kdevice_t;
 
  typedef struct kgetlog {
-	 kgltype_t		*kgl_type;
-	 uint32_t		kgl_typecnt;
-	 
-	 kutilization_t		*kgl_util;
-	 uint32_t		kgl_utilcnt;
+     kgltype_t        *kgl_type;
+     uint32_t          kgl_typecnt;
 
-	 ktemperature_t		*kgl_temp;
-	 uint32_t		kgl_tempcnt;
-	 
-	 kcapacitiy_t		kgl_cap;
-	 kconfiguration_t	kgl_conf;
-	 
-	 kstatistics_t		*kgl_stat;
-	 uint32_t		kgl_statcnt;
+     kutilization_t   *kgl_util;
+     uint32_t          kgl_utilcnt;
 
-	 char 			*kgl_msgs;
-	 uint32_t		kgl_msgscnt;
+     ktemperature_t   *kgl_temp;
+     uint32_t          kgl_tempcnt;
 
-	 klimits_t		kgl_limits;
-	 kdevicelog_t		kgl_log;
+     kcapacitiy_t      kgl_cap;
+     kconfiguration_t  kgl_conf;
+
+     kstatistics_t    *kgl_stat;
+     uint32_t          kgl_statcnt;
+
+     char             *kgl_msgs;
+     uint32_t          kgl_msgscnt;
+
+     klimits_t         kgl_limits;
+     kdevicelog_t      kgl_log;
  } kgetlog_t;
 
 
--- src/kinetic.h
+++ src/kinetic.h
@@ -1,60 +1,60 @@
 #ifndef _KINETIC_H
-#define _KINETIC_H 
+#define _KINETIC_H
 
-#include ""
-/** 
- * Kinetic Status Codes 
- */ 
+
+/**
+ * Kinetic Status Codes
+ */
 #define CSSC(cssc) Command_Status_StatusCode_##cssc
 typedef enum kstatus_code {
-	K_INVALID_SC	= CSSC(INVALID_STATUS_CODE),
-	K_OK		= CSSC(SUCCESS),
-	K_EREJECTED	= CSSC(NOT_ATTEMPTED),
-	K_EHMAC		= CSSC(HMAC_FAILURE),
-	K_EACCESS	= CSSC(NOT_AUTHORIZED),
-	K_EVERSION	= CSSC(VERSION_FAILURE),
-	K_EINTERNAL	= CSSC(INTERNAL_ERROR),
-	K_ENOHEADER	= CSSC(HEADER_REQUIRED),
-	K_ENOTFOUND 	= CSSC(NOT_FOUND),
-	K_EBADVERS	= CSSC(VERSION_MISMATCH),
-	K_EBUSY		= CSSC(SERVICE_BUSY),
-  	K_ETIMEDOUT	= CSSC(EXPIRED),
-	K_EDATA		= CSSC(DATA_ERROR),
-	K_EPERMDATA	= CSSC(PERM_DATA_ERROR),
-	K_EP2PCONN	= CSSC(REMOTE_CONNECTION_ERROR),
-	K_ENOSPACE	= CSSC(NO_SPACE),
-	K_ENOHMAC	= CSSC(NO_SUCH_HMAC_ALGORITHM),
-	K_EINVAL	= CSSC(INVALID_REQUEST),
-	K_EP2P		= CSSC(NESTED_OPERATION_ERRORS),
-	K_ELOCKED	= CSSC(DEVICE_LOCKED),
-	K_ENOTLOCKED	= CSSC(DEVICE_ALREADY_UNLOCKED),
-	K_ECONNABORTED	= CSSC(CONNECTION_TERMINATED),
-	K_EINVALBAT	= CSSC(INVALID_BATCH),
-	K_EHIBERNATE	= CSSC(HIBERNATE),
-	K_ESHUTDOWN	= CSSC(SHUTDOWN),
+    K_INVALID_SC   = CSSC(INVALID_STATUS_CODE),
+    K_OK           = CSSC(SUCCESS),
+    K_EREJECTED    = CSSC(NOT_ATTEMPTED),
+    K_EHMAC        = CSSC(HMAC_FAILURE),
+    K_EACCESS      = CSSC(NOT_AUTHORIZED),
+    K_EVERSION     = CSSC(VERSION_FAILURE),
+    K_EINTERNAL    = CSSC(INTERNAL_ERROR),
+    K_ENOHEADER    = CSSC(HEADER_REQUIRED),
+    K_ENOTFOUND    = CSSC(NOT_FOUND),
+    K_EBADVERS     = CSSC(VERSION_MISMATCH),
+    K_EBUSY        = CSSC(SERVICE_BUSY),
+    K_ETIMEDOUT    = CSSC(EXPIRED),
+    K_EDATA        = CSSC(DATA_ERROR),
+    K_EPERMDATA    = CSSC(PERM_DATA_ERROR),
+    K_EP2PCONN     = CSSC(REMOTE_CONNECTION_ERROR),
+    K_ENOSPACE     = CSSC(NO_SPACE),
+    K_ENOHMAC      = CSSC(NO_SUCH_HMAC_ALGORITHM),
+    K_EINVAL       = CSSC(INVALID_REQUEST),
+    K_EP2P         = CSSC(NESTED_OPERATION_ERRORS),
+    K_ELOCKED      = CSSC(DEVICE_LOCKED),
+    K_ENOTLOCKED   = CSSC(DEVICE_ALREADY_UNLOCKED),
+    K_ECONNABORTED = CSSC(CONNECTION_TERMINATED),
+    K_EINVALBAT    = CSSC(INVALID_BATCH),
+    K_EHIBERNATE   = CSSC(HIBERNATE),
+    K_ESHUTDOWN    = CSSC(SHUTDOWN),
 } kstatus_code_t;
 
 /**
  * Kinetic Status block
- */ 
+ */
 typedef struct kstatus {
-	kstatus_code_t	ks_code;
-	char *ks_message;
-	char *ks_detail;
-} kstatus_t;	
+    kstatus_code_t  ks_code;
+    char           *ks_message;
+    char           *ks_detail;
+} kstatus_t;
 
 /**
  * Kinetic Data Integrity supported algrithms.
  */
 #define CA(ca) Command_Algorithm_##ca
 enum kdi {
-	KDI_INVALID	= CA(INVALID_ALGORITHM),
-	KDI_SHA1	= CA(SHA1),
-	KDI_SHA2	= CA(SHA2),
-	KDI_SHA3	= CA(SHA3),
-	KDI_CRC32C	= CA(CRC32C),
-	KDI_CRC64	= CA(CRC64),
-	KDI_CRC32	= CA(CRC32).
+    KDI_INVALID = CA(INVALID_ALGORITHM),
+    KDI_SHA1    = CA(SHA1),
+    KDI_SHA2    = CA(SHA2),
+    KDI_SHA3    = CA(SHA3),
+    KDI_CRC32C  = CA(CRC32C),
+    KDI_CRC64   = CA(CRC64),
+    KDI_CRC32   = CA(CRC32).
 };
 
 /**
@@ -62,27 +62,27 @@ enum kdi {
  */
 #define CMT(cmt) Command_MessageType_##cmt
 typedef enum kmtype {
-	KMT_INVALID	= CMT(INVALID_MESSAGE_TYPE),
-	KMT_GET		= CMT(GET),		   
-	KMT_PUT		= CMT(PUT),		   
-	KMT_DEL		= CMT(DELETE),	   
-	KMT_GETNEXT 	= CMT(GETNEXT),	   
-	KMT_GETPREV	= CMT(GETPREVIOUS),	   
-	KMT_GETRANGE	= CMT(GETKEYRANGE),	   
-	KMT_GETVERS 	= CMT(GETVERSION),	   
-	KMT_SETUP 	= CMT(SETUP),	   
-	KMT_GETLOG 	= CMT(GETLOG),	   
-	KMT_SECURITY 	= CMT(SECURITY),	   
-	KMT_PUSHP2P 	= CMT(PEER2PEERPUSH),  
-	KMT_NOOP	= CMT(NOOP),	   
-	KMT_FLUSH	= CMT(FLUSHALLDATA),   
-	KMT_PINOP	= CMT(PINOP),	   
-	KMT_SCANMEDIA 	= CMT(MEDIASCAN),	   
-	KMT_OPTMEDIA 	= CMT(MEDIAOPTIMIZE),  
-	KMT_STARTBAT	= CMT(START_BATCH),	   
-	KMT_ENDBAT 	= CMT(END_BATCH),	   
-	KMT_ABORTBAT	= CMT(ABORT_BATCH),	   
-	KMT_SETPOWER	= CMT(SET_POWER_LEVEL),
+    KMT_INVALID   = CMT(INVALID_MESSAGE_TYPE),
+    KMT_GET       = CMT(GET),
+    KMT_PUT       = CMT(PUT),
+    KMT_DEL       = CMT(DELETE),
+    KMT_GETNEXT   = CMT(GETNEXT),
+    KMT_GETPREV   = CMT(GETPREVIOUS),
+    KMT_GETRANGE  = CMT(GETKEYRANGE),
+    KMT_GETVERS   = CMT(GETVERSION),
+    KMT_SETUP     = CMT(SETUP),
+    KMT_GETLOG    = CMT(GETLOG),
+    KMT_SECURITY  = CMT(SECURITY),
+    KMT_PUSHP2P   = CMT(PEER2PEERPUSH),
+    KMT_NOOP      = CMT(NOOP),
+    KMT_FLUSH     = CMT(FLUSHALLDATA),
+    KMT_PINOP     = CMT(PINOP),
+    KMT_SCANMEDIA = CMT(MEDIASCAN),
+    KMT_OPTMEDIA  = CMT(MEDIAOPTIMIZE),
+    KMT_STARTBAT  = CMT(START_BATCH),
+    KMT_ENDBAT    = CMT(END_BATCH),
+    KMT_ABORTBAT  = CMT(ABORT_BATCH),
+    KMT_SETPOWER  = CMT(SET_POWER_LEVEL),
 } kmtype_t;
 
 /**
@@ -90,32 +90,32 @@ typedef enum kmtype {
  */
 #define CS(cs) Command_Synchronization_##cs
 typedef enum kcachepolicy {
-	KC_INVALID 	= CS(INVALID_SYNCHRONIZATION),
-	KC_WT 		= CS(WRITETHROUGH),	 
-	KC_WB		= CS(WRITEBACK),	 
-	KC_FLUSH	= CS(FLUSH),
+    KC_INVALID = CS(INVALID_SYNCHRONIZATION),
+    KC_WT      = CS(WRITETHROUGH),
+    KC_WB      = CS(WRITEBACK),
+    KC_FLUSH   = CS(FLUSH),
 } kcachepolicy_t;
 
 /**
- * Kinetic Server Power Levels, 
+ * Kinetic Server Power Levels,
  * PAK: probably should be in management.h
  */
 #define CPL(cpl) Command_PowerLevel_##cpl
 typedef enum kpowerlevel {
-	KPL_INVALID	= CPL(INVALID_LEVEL),
-	KPL_NOMINAL 	= CPL(OPERATIONAL), 
-	KPL_HIBERNATE	= CPL(HIBERNATE),
-	KPL_SHUTDOWN	= CPL(SHUTDOWN),
-	KPL_FAIL	= CPL(FAIL),		
+    KPL_INVALID   = CPL(INVALID_LEVEL),
+    KPL_NOMINAL   = CPL(OPERATIONAL),
+    KPL_HIBERNATE = CPL(HIBERNATE),
+    KPL_SHUTDOWN  = CPL(SHUTDOWN),
+    KPL_FAIL      = CPL(FAIL),
 } kpowerlevel_t;
 
 
 kstatus_t ki_setclustervers(int ktd, int64_t vers);
 
 kstatus_t ki_get(int ktd, char *key, void **value,
-	      char **vers, char **tag, kdi_t **di);
+                 char **vers, char **tag, kdi_t **di);
 
 kstatus_t ki_getlog(int ktd, kgetlog_t *glog);
-	
-#endif /*  _KINETIC_H */
 
+
+#endif /*  _KINETIC_H */
