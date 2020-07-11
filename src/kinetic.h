fc353b7a830faa41920835296acaa76199ba23c8
Aldrin <akmontan@ucsc.edu>
2020-07-11 09:29:52 -0700

    Fixed naming; still an error to fix.
    Fixed typedefs of generated code, and changed enum aliases to be
    unnamed. Also, there is still an error due to a circular dependency.
    kgetlog_t is defined in getlog.h, but depends on types defined in
    kinetic.h. Likely this means the type definitions need to be together.

--- src/kinetic.h
+++ src/kinetic.h
@@ -2,10 +2,14 @@
 #define _KINETIC_H
 
 
+#include "getlog.h"
+
+
 /**
  * Kinetic Status Codes
  */
-#define CSSC(cssc) Command_Status_StatusCode_##cssc
+
+#define CSSC(cssc) COM__SEAGATE__KINETIC__PROTO__COMMAND__STATUS__STATUS_CODE__##cssc
 typedef enum kstatus_code {
     K_INVALID_SC   = CSSC(INVALID_STATUS_CODE),
     K_OK           = CSSC(SUCCESS),
@@ -46,22 +50,26 @@ typedef struct kstatus {
 /**
  * Kinetic Data Integrity supported algrithms.
  */
-#define CA(ca) Command_Algorithm_##ca
-enum kdi {
+#define CA(ca) COM__SEAGATE__KINETIC__PROTO__COMMAND__ALGORITHM__##ca
+
+typedef Com__Seagate__Kinetic__Proto__Command__Algorithm kditype_t;
+enum {
     KDI_INVALID = CA(INVALID_ALGORITHM),
-    KDI_SHA1    = CA(SHA1),
-    KDI_SHA2    = CA(SHA2),
-    KDI_SHA3    = CA(SHA3),
-    KDI_CRC32C  = CA(CRC32C),
-    KDI_CRC64   = CA(CRC64),
-    KDI_CRC32   = CA(CRC32).
+    KDI_SHA1    = CA(SHA1)             ,
+    KDI_SHA2    = CA(SHA2)             ,
+    KDI_SHA3    = CA(SHA3)             ,
+    KDI_CRC32C  = CA(CRC32C)           ,
+    KDI_CRC64   = CA(CRC64)            ,
+    KDI_CRC32   = CA(CRC32)            ,
 };
 
 /**
  * Kinetic Message Types, ie Kinetic Ops
  */
-#define CMT(cmt) Command_MessageType_##cmt
-typedef enum kmtype {
+#define CMT(cmt) COM__SEAGATE__KINETIC__PROTO__COMMAND__MESSAGE_TYPE__##cmt
+
+typedef Com__Seagate__Kinetic__Proto__Command__MessageType kmtype_t;
+enum {
     KMT_INVALID   = CMT(INVALID_MESSAGE_TYPE),
     KMT_GET       = CMT(GET),
     KMT_PUT       = CMT(PUT),
@@ -83,38 +91,43 @@ typedef enum kmtype {
     KMT_ENDBAT    = CMT(END_BATCH),
     KMT_ABORTBAT  = CMT(ABORT_BATCH),
     KMT_SETPOWER  = CMT(SET_POWER_LEVEL),
-} kmtype_t;
+};
 
 /**
  * Kinetic Cache Policies
  */
-#define CS(cs) Command_Synchronization_##cs
-typedef enum kcachepolicy {
+#define CS(cs) COM__SEAGATE__KINETIC__PROTO__COMMAND__SYNCHRONIZATION__##cs
+
+typedef Com__Seagate__Kinetic__Proto__Command__Synchronization kcachepolicy_t;
+enum {
     KC_INVALID = CS(INVALID_SYNCHRONIZATION),
-    KC_WT      = CS(WRITETHROUGH),
-    KC_WB      = CS(WRITEBACK),
-    KC_FLUSH   = CS(FLUSH),
-} kcachepolicy_t;
+    KC_WT      = CS(WRITETHROUGH)           ,
+    KC_WB      = CS(WRITEBACK)              ,
+    KC_FLUSH   = CS(FLUSH)                  ,
+};
 
 /**
  * Kinetic Server Power Levels,
  * PAK: probably should be in management.h
  */
-#define CPL(cpl) Command_PowerLevel_##cpl
-typedef enum kpowerlevel {
-    KPL_INVALID   = CPL(INVALID_LEVEL),
-    KPL_NOMINAL   = CPL(OPERATIONAL),
-    KPL_HIBERNATE = CPL(HIBERNATE),
-    KPL_SHUTDOWN  = CPL(SHUTDOWN),
-    KPL_FAIL      = CPL(FAIL),
-} kpowerlevel_t;
+#define CPLT(cplt) COM__SEAGATE__KINETIC__PROTO__COMMAND__POWER_LEVEL__##cplt
+
+typedef Com__Seagate__Kinetic__Proto__Command__PowerLevel kpltype_t;
+enum {
+  KPLT_INVALID     = CPLT(INVALID_LEVEL),
+  KPLT_OPERATIONAL = CPLT(OPERATIONAL)  ,
+  KPLT_HIBERNATE   = CPLT(HIBERNATE)    ,
+  KPLT_SHUTDOWN    = CPLT(SHUTDOWN)     ,
+  KPLT_FAIL        = CPLT(FAIL)         ,
+};
 
 
 kstatus_t ki_setclustervers(int ktd, int64_t vers);
 
 kstatus_t ki_get(int ktd, char *key, void **value,
-                 char **vers, char **tag, kdi_t **di);
+                 char **vers, char **tag, kditype_t **di);
 
+// TODO: this is a circular dependency and needs to be resolved
 kstatus_t ki_getlog(int ktd, kgetlog_t *glog);
 
 
