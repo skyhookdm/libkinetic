1681f688232d230ebe9e93de6be1b0b6134e5f06
Aldrin <akmontan@ucsc.edu>
2020-07-14 09:12:06 -0700

    resolved conflicts in kinetic.h
    These conflicts were missed and committed. Conflicts were primarily on
    types that were moved to protocol_types.h and the addition of types that
    used to be in protocol_interface.h (which has been removed).

--- src/kinetic.h
+++ src/kinetic.h
@@ -1,9 +1,9 @@
 #ifndef _KINETIC_H
 #define _KINETIC_H
 
-<<<<<<< HEAD
 
 #include "protocol_types.h"
+#include "message.h"
 
 
 //Forward declarations of operation-specific structs used in the API
@@ -22,96 +22,6 @@ enum kresult_code {
 struct kbuffer {
 	size_t	len;
 	void   *base;
-=======
-#include "getlog.h"
-#include "message.h"
-
-/** 
- * Kinetic Status Codes 
- */ 
-#define CSSC(cssc) COM__SEAGATE__KINETIC__PROTO__COMMAND__STATUS__STATUS_CODE__##cssc
-typedef enum kstatus_code {
-    K_INVALID_SC   = CSSC(INVALID_STATUS_CODE),
-    K_OK           = CSSC(SUCCESS),
-    K_EREJECTED    = CSSC(NOT_ATTEMPTED),
-    K_EHMAC        = CSSC(HMAC_FAILURE),
-    K_EACCESS      = CSSC(NOT_AUTHORIZED),
-    K_EVERSION     = CSSC(VERSION_FAILURE),
-    K_EINTERNAL    = CSSC(INTERNAL_ERROR),
-    K_ENOHEADER    = CSSC(HEADER_REQUIRED),
-    K_ENOTFOUND    = CSSC(NOT_FOUND),
-    K_EBADVERS     = CSSC(VERSION_MISMATCH),
-    K_EBUSY        = CSSC(SERVICE_BUSY),
-    K_ETIMEDOUT    = CSSC(EXPIRED),
-    K_EDATA        = CSSC(DATA_ERROR),
-    K_EPERMDATA    = CSSC(PERM_DATA_ERROR),
-    K_EP2PCONN     = CSSC(REMOTE_CONNECTION_ERROR),
-    K_ENOSPACE     = CSSC(NO_SPACE),
-    K_ENOHMAC      = CSSC(NO_SUCH_HMAC_ALGORITHM),
-    K_EINVAL       = CSSC(INVALID_REQUEST),
-    K_EP2P         = CSSC(NESTED_OPERATION_ERRORS),
-    K_ELOCKED      = CSSC(DEVICE_LOCKED),
-    K_ENOTLOCKED   = CSSC(DEVICE_ALREADY_UNLOCKED),
-    K_ECONNABORTED = CSSC(CONNECTION_TERMINATED),
-    K_EINVALBAT    = CSSC(INVALID_BATCH),
-    K_EHIBERNATE   = CSSC(HIBERNATE),
-    K_ESHUTDOWN    = CSSC(SHUTDOWN),
-} kstatus_code_t;
-
-/**
- * Kinetic Status block
- */
-typedef struct kstatus {
-	kstatus_code_t	ks_code;
-	char 		*ks_message;
-	char		*ks_detail;
-} kstatus_t;	
-
-/**
- * Kinetic Data Integrity supported algrithms.
- */
-#define CA(ca) COM__SEAGATE__KINETIC__PROTO__COMMAND__ALGORITHM__##ca
-
-typedef Com__Seagate__Kinetic__Proto__Command__Algorithm kditype_t;
-enum {
-	KDI_INVALID = CA(INVALID_ALGORITHM),
-	KDI_SHA1    = CA(SHA1)             ,
-	KDI_SHA2    = CA(SHA2)             ,
-	KDI_SHA3    = CA(SHA3)             ,
-	KDI_CRC32C  = CA(CRC32C)           ,
-	KDI_CRC64   = CA(CRC64)            ,
-	KDI_CRC32   = CA(CRC32)            ,
-};
-
-/**
- * Kinetic Message Types, ie Kinetic Ops
- */
-#define CMT(cmt) COM__SEAGATE__KINETIC__PROTO__COMMAND__MESSAGE_TYPE__##cmt
-
-typedef Com__Seagate__Kinetic__Proto__Command__MessageType kmtype_t;
-enum {
-	KMT_INVALID   = CMT(INVALID_MESSAGE_TYPE),
-	KMT_GET       = CMT(GET),
-	KMT_PUT       = CMT(PUT),
-	KMT_DEL       = CMT(DELETE),
-	KMT_GETNEXT   = CMT(GETNEXT),
-	KMT_GETPREV   = CMT(GETPREVIOUS),
-	KMT_GETRANGE  = CMT(GETKEYRANGE),
-	KMT_GETVERS   = CMT(GETVERSION),
-	KMT_SETUP     = CMT(SETUP),
-	KMT_GETLOG    = CMT(GETLOG),
-	KMT_SECURITY  = CMT(SECURITY),
-	KMT_PUSHP2P   = CMT(PEER2PEERPUSH),
-	KMT_NOOP      = CMT(NOOP),
-	KMT_FLUSH     = CMT(FLUSHALLDATA),
-	KMT_PINOP     = CMT(PINOP),
-	KMT_SCANMEDIA = CMT(MEDIASCAN),
-	KMT_OPTMEDIA  = CMT(MEDIAOPTIMIZE),
-	KMT_STARTBAT  = CMT(START_BATCH),
-	KMT_ENDBAT    = CMT(END_BATCH),
-	KMT_ABORTBAT  = CMT(ABORT_BATCH),
-	KMT_SETPOWER  = CMT(SET_POWER_LEVEL),
->>>>>>> origin/develop
 };
 
 struct kresult_buffer {
