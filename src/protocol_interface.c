ee7313a0ac0b82ef7bc3236fa9fc883fbf95f369
Aldrin <akmontan@ucsc.edu>
2020-07-20 12:40:46 -0700

    Added implementations of some helpers
    ki_getaseq and ki_setseq have innefficient, temporary implementations.
    The inefficiencies are in that they deserialize and allocate the kinetic
    message and commands. These will eventually be replaced by something
    that walks the packed protobuf bytes looking for the fields of interest.

--- src/protocol_interface.c
+++ src/protocol_interface.c
@@ -20,6 +20,7 @@
 #include "kinetic.h"
 #include "protocol_types.h"
 #include "message.h"
+#include "kio.h"
 
 /**
  * This file holds the highest-level of the implementation of an interface to the protocol. We want
@@ -248,4 +249,131 @@ void destroy_command(void *unpacked_cmd) {
 		(kproto_cmd_t *) unpacked_cmd,
 		mem_allocator
 	);
+}
+
+// TODO: this is a helper fn, picked from protobuf-c source, to assist in walking protobuf code
+/* NOTE: not yet used, so just commenting out
+static size_t
+parse_tag_and_wiretype(size_t len, const uint8_t *data, uint32_t *tag_out, ProtobufCWireType *wiretype_out) {
+	/* Bit fields for reference
+	 *     0xf8: 1111 1000    0x80: 1000 0000
+	 *     0x07: 0000 0111    0x7f: 0111 1111
+	 *
+	unsigned max_rv          = len > 5 ? 5 : len;
+	uint32_t tag             = (data[0] & 0x7f) >> 3;
+	unsigned half_byte_width = 4;
+	unsigned rv;
+
+	// 0 is not a valid tag value
+	if ((data[0] & 0xf8) == 0) { return 0; }
+
+	// grab the field's wiretype (type used for serializing to the wire)
+	*wiretype_out = data[0] & 7;
+
+	// significant bit tells us if the next byte is part of the tag
+	if ((data[0] & 0x80) == 0) {
+		*tag_out = tag;
+		return 1;
+	}
+
+	// for each byte, up to max_rv (max of 5 and len)
+	for (rv = 1; rv < max_rv; rv++) {
+
+		// if the significant bit is set, accumulate into the tag
+		if (data[rv] & 0x80) {
+			tag |= (data[rv] & 0x7f) << half_byte_width;
+			half_byte_width += 7;
+		}
+
+		// otherwise, accumulate this one and return how many bytes were accumulated
+		else {
+			tag |= data[rv] << half_byte_width;
+			*tag_out = tag;
+			return rv + 1;
+		}
+	}
+
+	// error: bad header
+	return 0;
+}
+*/
+
+// TODO: this is going to be used for results of walking the protobuf data
+struct protobuf_loc {
+	size_t byte_offset;
+	size_t wiretype_len;
+	uint8_t (*unpack_field)(uint8_t *data_buffer, size_t byte_offset, size_t len);
+	uint8_t (*pack_field)(uint8_t *data_buffer, size_t byte_offset, size_t len);
+};
+
+
+// TODO: it's really painful, but this is implemented for functionality first. will remove
+// unnecessary allocations later
+int64_t ki_getaseq(struct kiovec *msg, int msgcnt) {
+	int64_t ack_seq = -1;
+
+	// walk the message first
+	struct kresult_message unpack_result = unpack_kinetic_message(msg->kiov_base, msg->kiov_len);
+	if (unpack_result.result_code == FAILURE) {
+		//TODO: we won't allocate in the future, but we should figure out what to do for errors
+		return -1;
+	}
+
+	kproto_msg_t *tmp_msg = (kproto_msg_t *) unpack_result.result_message;
+
+
+	// then walk the command
+	kproto_cmd_t *tmp_cmd = unpack_kinetic_command(tmp_msg->commandbytes);
+
+	// extract the ack sequence field
+	if (tmp_cmd->header->has_acksequence) {
+		ack_seq = tmp_cmd->header->acksequence;
+	}
+
+
+	// TODO: since we allocate currently, we need to clean up
+	destroy_command(tmp_cmd);
+	destroy_message(tmp_msg);
+
+	return ack_seq;
+}
+
+// TODO: it's really painful, but this is implemented for functionality first. will remove
+// unnecessary allocations later
+void ki_setseq(struct kiovec *msg, int msgcnt, int64_t seq) {
+	// walk the message first
+	struct kresult_message unpack_result = unpack_kinetic_message(msg->kiov_base, msg->kiov_len);
+	if (unpack_result.result_code == FAILURE) {
+		//TODO: we won't allocate in the future, but we should figure out what to do for errors
+		//return -1;
+		;
+	}
+
+	kproto_msg_t *tmp_msg = (kproto_msg_t *) unpack_result.result_message;
+
+
+	// then walk the command
+	kproto_cmd_t *tmp_cmd = unpack_kinetic_command(tmp_msg->commandbytes);
+
+	// extract the ack sequence field
+	tmp_cmd->header->has_acksequence = 1;
+	tmp_cmd->header->acksequence     = seq;
+
+	// pack field (TODO: currently this packs the whole thing, but eventually we would like to pack
+	// just the new field)
+	tmp_msg->commandbytes = pack_kinetic_command(tmp_cmd);
+
+	enum kresult_code repack_result = pack_kinetic_message(
+		tmp_msg,
+		&(msg->kiov_base),
+		&(msg->kiov_len)
+	);
+
+	// TODO: since we allocate currently, we need to clean up
+	destroy_command(tmp_cmd);
+	destroy_message(tmp_msg);
+}
+
+int32_t ki_msglen(struct kiovec *msg_hdr) {
+	return 0;
 }
