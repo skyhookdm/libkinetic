69ffa74cd90217145575f62a4de2d220b12eb6d6
Aldrin <aldrin.montana@seagate.com>
2020-09-04 13:04:18 -0700

    util.bash: added and updated functions
    These are functions for calculating crc32 and other convenience
    functions to keep usage across test cases standard

--- tests/blackbox-bash/functions/util.bash
+++ tests/blackbox-bash/functions/util.bash
@@ -6,15 +6,28 @@ nanoseconds_to_seconds() {
     echo $(perl -e 'print($ARGV[0]/1e9."\n");' ${nanoseconds})
 }
 
-random_keyval() {
-    val_len=${1:-32}
+binary_to_hex() {
+    input_val=${1:?"Error: No binary input provided to convert to hex"}
 
-    key_val=""
+    echo -n ${input_val} | xxd -p -u -c1
+}
 
-    for val_ndx in {1..$val_len}; do
-        let val_symbol=$((val_ndx % 26))+65
-        key_val="${key_val}${val_symbol}"
-    done
+binary_to_escaped_hex() {
+    input_val=${1:?"Error: No binary input provided to convert to escaped hex"}
 
-    echo ${key_val}
+    formatted_val=$(echo -n ${input_val} | xxd -p -u -c1)
+
+    # use perl to join each array element with the string "\x" to denote a hex string
+    perl -e 'print("\\x".join("\\x", @ARGV));' ${formatted_val}
+}
+
+compute_crc32() {
+    input_val=${1:?"Error: CRC32 checksum requires an input value"}
+
+    if [[ -z $(which /usr/bin/crc32) ]]; then
+        echo "Error: Unable to find '/usr/bin/crc32'"
+        return 1
+    fi
+
+    echo -n ${input_val} | /usr/bin/crc32 /dev/stdin
 }
