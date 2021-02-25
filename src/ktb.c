25d190ee8da06565ab8614f17fd7ec2184930d7e
Philip Kufeldt <philip.kufeldt@seagate.com>
2021-02-24 21:56:40 -0800

    Missed a file for Version support checkin, #62

--- src/ktb.c
+++ src/ktb.c
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2020-2021 Seagate Technology LLC.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at
+ * https://mozilla.org/MP:/2.0/.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but is provided AS-IS, WITHOUT ANY WARRANTY; including without
+ * the implied warranty of MERCHANTABILITY, NON-INFRINGEMENT or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the Mozilla Public
+ * License for more details.
+ *
+ */
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -102,6 +117,8 @@ ktb_buf_len(ktype_t t)
 		/* opaque type, use the backing type */
 		return((uint32_t)sizeof(kb_t));
 	case KGETLOG_T:
+		return((uint32_t)sizeof(kgetlog_t));
+	case KVERSION_T:
 		return((uint32_t)sizeof(kgetlog_t));
 	default:
 		return(0);
