3a8bce78c9640afc146aaf9e63ce49a0e807f4bb
skyhook-vm-user <skyhook-vm-user@skyhook>
2021-05-13 21:57:27 +0000

    keys.bash: minor updates and improvement
    the improvement is that `sequential_ids` had to be followed with a call
    to `normalize_keyname` to be normalized to a lexicographic prefix. But,
    now the `-w` flag is used with `seq` to do the normalization for us

--- scripts/functions/keys.bash
+++ scripts/functions/keys.bash
@@ -16,19 +16,20 @@
 
 
 random_ids() {
-    num_ids=$1
+    num_ids=${1}
 
-    let start_val=$RANDOM step_val=$RANDOM
-    let stop_val=($step_val*$num_ids)+$start_val-1
+    let start_val=${RANDOM}
+    let step_val=${RANDOM}
+    stop_val=$(( (${step_val} * ${num_ids}) + ${start_val} - 1 ))
 
-    echo $(seq $start_val $step_val $stop_val)
+    echo $(seq ${start_val} ${step_val} ${stop_val})
 }
 
 sequential_ids() {
-    start_id=$1
-    num_ids=$2
+    start_id=${1}
+    num_ids=${2}
 
-    echo $(seq $start_id 1 $num_ids)
+    echo $(seq -w ${start_id} 1 ${num_ids})
 }
 
 normalize_keyname() {
