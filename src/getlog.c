4772f9fc60295410d94b63a786ba7363308c8765
akmontan <akmontan@ucsc.edu>
2021-06-25 22:54:52 +0000

    getlog.c: (#50) added ktb memory management
    to accommodate new allocations and the protobuf struct, a new struct is
    defined in this source file that points to the getlog struct (kgetlog_t)
    and the protobuf struct. This struct is allocated and then tracked using
    ktb_ctx.

--- src/getlog.c
+++ src/getlog.c
@@ -31,6 +31,54 @@
 #include "kinetic_internal.h"
 #include "protocol_interface.h"
 
+
+/**
+ * getlog_ctx and destroy_getlog_ctx are for managing a kgetlog_t structure
+ * and its associated protobuf structure. Expected usage of these is that a
+ * `struct getlog_ctx` will be allocated and tracked by a KTB context (`ktb_ctx`).
+ * When the getlog_ctx is ready to be released (or on failure to add it to
+ * a ktb, `destroy_getlog_ctx` is invoked and the allocated getlog_ctx is provided.
+ * The `destroy_getlog_ctx` will free each pointer (the protobuf struct and
+ * pointers in the getlog struct that point to the protobuf struct), then free
+ * the allocated `getlog_ctx`, itself.
+ */
+typedef struct getlog_ctx {
+	kproto_cmd_t *response_cmd;
+	kgetlog_t    *getlog_data;
+} getlog_ctx_t;
+
+void destroy_getlog_ctx(void *ctx_ptr) {
+	// If we have nothing to do; return.
+	if (!ctx_ptr) { return; }
+
+	getlog_ctx_t *ctx_data    = (getlog_ctx_t *) ctx_ptr;
+	kgetlog_t    *getlog_data = ctx_data->getlog_data;
+
+	// First, release the allocated memory for the protobuf message data
+	if (ctx_data->response_cmd) { destroy_command(ctx_data->response_cmd); }
+
+	// Second, release each array of pointers that now point to free'd memory
+	if (getlog_data->kgl_util) { KI_FREE(getlog_data->kgl_util); }
+	if (getlog_data->kgl_temp) { KI_FREE(getlog_data->kgl_temp); }
+	if (getlog_data->kgl_stat) { KI_FREE(getlog_data->kgl_stat); }
+
+	if (getlog_data->kgl_conf.kcf_serial) {
+		KI_FREE(getlog_data->kgl_conf.kcf_serial);
+	}
+
+	if (getlog_data->kgl_conf.kcf_wwn) {
+		KI_FREE(getlog_data->kgl_conf.kcf_wwn);
+	}
+
+	if (getlog_data->kgl_conf.kcf_interfaces) {
+		KI_FREE(getlog_data->kgl_conf.kcf_interfaces);
+	}
+
+	// Finally, free the ctx pointer, itself
+	KI_FREE(ctx_ptr);
+}
+
+
 /**
  * ki_getlog(int ktd, kgetlog_t *glog)
  *
@@ -235,12 +283,8 @@ ki_getlog(int ktd, kgetlog_t *glog)
 	krc = extract_getlog(&kmresp, &glog2);
 
 	// if successful, memcpy into glog (no separate validation at the moment)
-	if (krc == K_OK) {
-		memcpy(glog, &glog2, sizeof(kgetlog_t));
-	}
-	else {
-		glog2.destroy_protobuf(&glog2);
-	}
+	// otherwise    , extract_getlog should have cleaned up glog2
+	if (krc == K_OK) { memcpy(glog, &glog2, sizeof(kgetlog_t)); }
 
 	/* clean up */
  glex_resp:
@@ -425,7 +469,7 @@ int extract_configuration(kgetlog_t *getlog_data, kproto_configuration_t *config
 	getlog_data->kgl_conf.kcf_model  = config->model;
 
 	// NOTE: we may want to have a different representation of `bytes` fields
-	// NOTE: serial and wwn are `free`d in `destroy_protobuf_getlog`
+	// NOTE: serial and wwn are `free`d in `destroy_getlog_ctx`
 	if (config->has_serialnumber) {
 		getlog_data->kgl_conf.kcf_serial = helper_bytes_to_str(config->serialnumber);
 	}
@@ -481,33 +525,6 @@ void extract_limits(kgetlog_t *getlog_data, kproto_limits_t *limits) {
 	}
 }
 
-// This may get a partially defined structure if we hit an error during the construction.
-// NOTE: it's possible we will only want to set this function if getlog_data was successfully
-//       created (and so on errors, everything gets free'd immediately)
-void destroy_protobuf_getlog(kgetlog_t *getlog_data) {
-	// Don't do anything if we didn't get a valid pointer
-	if (!getlog_data) { return; }
-
-	// first destroy the allocated memory for the message data
-	destroy_command(getlog_data->kgl_protobuf);
-
-	// then free arrays of pointers that point to the message data
-	if (getlog_data->kgl_util) { KI_FREE(getlog_data->kgl_util); }
-	if (getlog_data->kgl_temp) { KI_FREE(getlog_data->kgl_temp); }
-	if (getlog_data->kgl_stat) { KI_FREE(getlog_data->kgl_stat); }
-
-	if (getlog_data->kgl_conf.kcf_serial) {
-		KI_FREE(getlog_data->kgl_conf.kcf_serial);
-	}
-
-	if (getlog_data->kgl_conf.kcf_wwn) {
-		KI_FREE(getlog_data->kgl_conf.kcf_wwn);
-	}
-
-	if (getlog_data->kgl_conf.kcf_interfaces) {
-		KI_FREE(getlog_data->kgl_conf.kcf_interfaces);
-	}
-}
 
 kstatus_t
 extract_getlog(struct kresult_message *resp_msg, kgetlog_t *getlog_data)
@@ -527,14 +544,21 @@ extract_getlog(struct kresult_message *resp_msg, kgetlog_t *getlog_data)
 		debug_printf("extract_getlog: resp cmd unpack\n");
 		return krc;
 	}
-	getlog_data->kgl_protobuf = resp_cmd;
 
-	// set destructor to be called later
-	getlog_data->destroy_protobuf = destroy_protobuf_getlog;
+	// try allocating this first to simplify the error modes
+	// on error, only the unpacked command has been allocated so far.
+	getlog_ctx_t *ctx_pair = (getlog_ctx_t *) KI_MALLOC(sizeof(getlog_ctx_t));
+	if (!ctx_pair) {
+		destroy_command(resp_cmd);
+		debug_printf("extract_getlog: unable to allocate context\n");
+		return krc;
+	}
 
-	// extract the kinetic response status; 
-	// copy the data for data independence
-        krc = extract_cmdstatus_code(resp_cmd);
+	// make sure we don't erroneously try to cleanup garbage pointers
+	memset(ctx_pair, 0, sizeof(getlog_ctx_t));
+
+	// extract the status. On failure, skip to cleanup
+	krc = extract_cmdstatus_code(resp_cmd);
 	if (krc != K_OK) {
 		debug_printf("extract_getlog: status\n");
 		goto extract_glex;
@@ -546,11 +570,26 @@ extract_getlog(struct kresult_message *resp_msg, kgetlog_t *getlog_data)
 		goto extract_glex;
 	}
 
-	// begin extraction of command body into getlog structure
-	kproto_getlog_t *resp = resp_cmd->body->getlog;
+	// Since everything seemed successful, let's pop this data on our cleaning stack
+	// NOTE: ctx_pair is free-d by destroy_getlog_ctx
+	*ctx_pair = (getlog_ctx_t) {
+		 .response_cmd = resp_cmd
+		,.getlog_data  = getlog_data
+	};
+
+	krc = ki_addctx(getlog_data, (void *) ctx_pair, destroy_getlog_ctx);
+	if (krc != K_OK) {
+		debug_printf("extract_getlog: failed to add context\n");
+		goto extract_glex;
+	}
+
+	// ------------------------------
+	// begin extraction of command data
 
 	// 0 is success, < 0 is failure. Use this for all the extract functions
 	int extract_result;
+
+	kproto_getlog_t *resp = resp_cmd->body->getlog;
 
 	getlog_data->kgl_typecnt = resp->n_types;
 	getlog_data->kgl_type    = resp->types;
@@ -615,14 +654,11 @@ extract_getlog(struct kresult_message *resp_msg, kgetlog_t *getlog_data)
 	return krc;
 
  extract_glex:
-	// set this so `destroy_protobuf_getlog` can correctly destroy it
-	destroy_protobuf_getlog(getlog_data);
+	debug_printf("extract_getlog: error exit\n");
+	destroy_getlog_ctx(ctx_pair);
 
 	// Just make sure we don't return an ok message
-	if (krc == K_OK) {
-		debug_printf("extract_getlog: error exit");
-		krc = K_EINTERNAL;
-	}
+	if (krc == K_OK) { krc = K_EINTERNAL; }
 
 	return krc;
 }
